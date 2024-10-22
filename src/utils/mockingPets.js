4852897d91a21b7621e033a283f3b46880cb3e63
Jaqui <jaqui.mz08.jm@gmail.com>
2024-10-22 18:09:18 -0300

    Documento mockingPets.js en utils

---
+++ src/utils/mockingPets.js
@@ -0,0 +1,20 @@
+const { faker } = require('@faker-js/faker');
+
+function generateMockPets(count = 100) {
+  const pets = [];
+
+  for (let i = 0; i < count; i++) {
+    pets.push({
+      _id: faker.database.mongodbObjectId(), // Simula un ID de MongoDB
+      name: faker.animal.cat(), // Nombre de un animal (cat, dog, etc.)
+      species: faker.helpers.arrayElement(['cat', 'dog', 'bird']),
+      age: faker.datatype.number({ min: 1, max: 15 }),
+      breed: faker.animal.type(),
+      adopted: false,
+    });
+  }
+
+  return pets;
+}
+
+module.exports = { generateMockPets };
