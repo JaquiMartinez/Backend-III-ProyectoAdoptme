fd7653af9568ec3dbc820a57b4a24dcb2b1faf1c
Jaqui <jaqui.mz08.jm@gmail.com>
2024-10-28 18:24:57 -0300

    actualización de users.router

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,13 +1,23 @@
 import { Router } from 'express';
 import usersController from '../controllers/users.controller.js';
+import Users from '../dao/Users.dao.js';
 
 const router = Router();
+const usersDao = new Users();
 
-router.get('/',usersController.getAllUsers);
+router.get('/', usersController.getAllUsers);
+router.get('/:uid', usersController.getUser);
+router.put('/:uid', usersController.updateUser);
+router.delete('/:uid', usersController.deleteUser);
 
-router.get('/:uid',usersController.getUser);
-router.put('/:uid',usersController.updateUser);
-router.delete('/:uid',usersController.deleteUser);
-
+// Nueva ruta: Registro de usuario
+router.post('/register', async (req, res, next) => {
+  try {
+    const user = await usersDao.save(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
